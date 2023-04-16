bd16baf2a8e616d74ee2c3bb21f2010224b4f212
SorryYearnt <setsuketsuka@foxmail.com>
2023-04-16 23:22:05 +0800

    Initialization

---
+++ Promise.js
@@ -0,0 +1,167 @@
+class Promise {
+	#PromiseState = 'pending';
+	#PromiseResult = void 0;
+	#thenList = [];
+	#resolvedBy = this;
+
+	#asynchronousProcess = (process) => {
+		setTimeout(process, 0);
+	}
+	#actThen() {
+		for (let i = 0; i < this.#thenList.length; i++) {
+			this.#thenList[i]();
+		}
+	}
+	#checkResolved(process, parentResolvedBy) {
+		return value => {
+			if (parentResolvedBy !== this.#resolvedBy) {
+				return;
+			}
+			this.#resolvedBy = value;
+			return process(value);
+		};
+	}
+
+	#resolve = value => {
+		let typeofValue = typeof value;
+		if (typeofValue === 'object' || typeofValue === 'function') {
+			let then;
+			try {
+				then = value?.then;
+			}
+			catch (exception) {
+				this.#reject(exception);
+				return;
+			}
+			if (typeof then === 'function') {
+				if (value === this) {
+					this.#resolvedBy = new TypeError('Chaining cycle detected for promise #<Promise>');
+					this.#reject(this.#resolvedBy);
+					return;
+				}
+				const resolutionFunc = this.#checkResolved(this.#resolve, value);
+				const rejectionFunc = this.#checkResolved(this.#reject, value);
+				this.#asynchronousProcess(() => {
+					try {
+						then.call(value, resolutionFunc, rejectionFunc);
+					}
+					catch (exception) {
+						rejectionFunc(exception);
+					}
+				});
+				return;
+			}
+		}
+		this.#PromiseResult = value;
+		this.#PromiseState = 'fulfilled';
+		this.#actThen();
+	}
+	#reject = (reason) => {
+		this.#PromiseResult = reason;
+		this.#PromiseState = 'rejected';
+		this.#actThen();
+	}
+
+	constructor(executor) {
+		if (typeof executor !== 'function') {
+			if (typeof executor === 'object' && executor) {
+				let className = executor.constructor?.name;
+				let protoToString = Object.prototype.toString;
+				if (executor.toString && executor.toString === protoToString && className) {
+					executor = `#<${className}>`;
+				}
+				else {
+					executor = protoToString.call(executor);
+				}
+			}
+			else {
+				if (executor) {
+					executor = executor.toString();
+				}
+			}
+			throw new TypeError(`Promise resolver ${executor} is not a function`);
+		}
+		const resolutionFunc = this.#checkResolved(this.#resolve, this);
+		const rejectionFunc = this.#checkResolved(this.#reject, this);
+		try {
+			executor(resolutionFunc, rejectionFunc);
+		}
+		catch (exception) {
+			rejectionFunc(exception);
+		}
+	}
+	/*本代码由SorryYearnt编写，转载请注明出处。This code was written by SorryYearnt. Please indicate the source when reprinting. このコードはSorryYearntによって書かれており、転載は出典を明記してください。*/
+
+	then(onFulfilled, onRejected) {
+		if (typeof onFulfilled !== 'function') {
+			onFulfilled = x => x;
+		}
+		if (typeof onRejected !== 'function') {
+			onRejected = x => {
+				throw x
+			};
+		}
+		let result = new Promise((resolutionFunc, rejectionFunc) => {
+			const setAction = () => {
+				this.#asynchronousProcess(() => {
+					try {
+						if (this.#PromiseState === 'fulfilled') {
+							resolutionFunc(onFulfilled(this.#PromiseResult));
+						}
+						else {
+							resolutionFunc(onRejected(this.#PromiseResult));
+						}
+					}
+					catch (exception) {
+						rejectionFunc(exception);
+					}
+				})
+			};
+			if (this.#PromiseState !== 'pending') {
+				setAction();
+			}
+			else {
+				this.#thenList.push(setAction);
+			}
+		});
+		return result;
+	}
+	catch(onRejected) {
+		this.then(void 0, onRejected);
+	}
+
+	static resolve(value) {
+		if (value instanceof Promise) {
+			return value;
+		}
+		return new Promise(resolutionFunc => resolutionFunc(value));
+	}
+	static reject(reason) {
+		return new Promise((resolutionFunc, rejectionFunc) => rejectionFunc(reason));
+	}
+	static all(iterable) {
+		return new Promise((resolutionFunc, rejectionFunc) => {
+			const list = Array.from(iterable, element => Promise.resolve(element));
+			length = list.length;
+			if (length === 0) {
+				resolutionFunc(list);
+			}
+			let count = 0;
+			list.forEach((element, index) => {
+				element.then(value => {
+					list[index] = value;
+					count++;
+					if (count === length) {
+						resolutionFunc(list);
+					}
+				}, reason => rejectionFunc(reason));
+			});
+		});
+	}
+	static race(iterable) {
+		return new Promise((resolutionFunc, rejectionFunc) => {
+			const list = Array.from(iterable, element => Promise.resolve(element));
+			list.forEach(element => element.then(value => resolutionFunc(value), reason => rejectionFunc(reason)));
+		});
+	}
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# Promise
+
+[^_^]: # (![访问统计](https://vbr.wocr.tk/badge?page_id=SorryYearnt.promise&color=00cf00))
+![访问统计](https://visitor-badge.laobi.icu/badge?page_id=SorryYearnt.promise&right_color=green&left_text=Visitors)
+
+![访问统计](https://count.getloli.com/get/@SorryYearnt.promise?theme=moebooru)
+
+## 简介
+
+使用setTimeout模拟Promise，模仿Google Chrome的行为，通过Promises/A+测试。
+
+## 许可声明
+
+本代码由SorryYearnt编写，转载请注明出处。
+
+This code was written by SorryYearnt. Please indicate the source when reprinting.
+
+このコードはSorryYearntによって書かれており、転載は出典を明記してください。
+
+## 参考及使用项目
+
+* [promises-aplus/promises-tests](https://github.com/promises-aplus/promises-tests)
+
+---
+<p align=right>Make China great again!</p>
